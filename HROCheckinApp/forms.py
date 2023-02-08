3bf1b84129e879955674fdb29962c6cddb3515aa
Falco-0941881 <0941881@hr.nl>
2023-02-08 12:34:02 +0100

    RESET

--- HROCheckinApp/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from HROCheckinApp.models import Student
-
-class StudentForm(forms.ModelForm):
-    class Meta:
-        model = Student
-        fields = ("first_name", "last_name", "password",)	# NOTE: the trailing comma is required
--- HROCheckinApp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from HROCheckinApp import views
-
-urlpatterns = [
-    path("", views.home, name="home"),
-    path("login/", views.login, name="login"),
-]
